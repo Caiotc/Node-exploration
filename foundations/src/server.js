955428a3126afeae836f7751ebf58e587dfa938b
Caio Theodoro de Carvalho <caio.carvalho@coxautoinc.com>
2023-08-16 03:22:27 -0300

    almost a simple crud

--- foundations/src/server.js
+++ foundations/src/server.js
@@ -1,9 +1,27 @@
 import http from "node:http";
 
-const users = [];
-const server = http.createServer((req, res) => {
+const users = [
+  {
+    id: 1,
+    name: "Darth",
+    email: "a@a.com",
+  },
+];
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
   console.log(method, url);
+
+  const Buffers = [];
+  for await (const chunk of req) {
+    Buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(Buffers).toString());
+  } catch {
+    req.body = null;
+  }
+  console.log(req.body);
 
   if (method === "GET" && url === "/users") {
     return res
@@ -12,11 +30,8 @@ const server = http.createServer((req, res) => {
   }
 
   if (method === "POST" && url === "/users") {
-    users.push({
-      id: 1,
-      name: "Darth",
-      email: "a@a.com",
-    });
+    users.push({ id: users[users.length - 1].id + 1, ...req.body });
+
     return res.writeHead(201).end("create users");
   }
   return res.writeHead(404).end();
